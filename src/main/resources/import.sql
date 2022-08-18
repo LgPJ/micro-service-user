fec432eab5e762b65069c5b6d476a7cae89266fa
LgPJ <guillermobarreto100@gmail.com>
2022-08-18 16:31:19 -0400

    configurando datos en H2

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO users (username, password, is_active, name, email) VALUES('@andres', '159753', 1, 'Andres', 'andres@gmail.com');
-INSERT INTO users (username, password, is_active, name, email) VALUES('@Luis', '123456', 1, 'Luis', 'luis@gmail.com');
+INSERT INTO users (username, password, is_active, name, email) VALUES('@andres', '2a$10$Kcgp1fmpUQkWN0okwQ15Wua31yCfPQg32haXk7uAZ9omt/EFe5j5O', 1, 'Andres', 'andres@gmail.com');
+INSERT INTO users (username, password, is_active, name, email) VALUES('@Luis', '2a$10$S6twYMXEN7nt031i5/e.t.rtshc7SAleOfcmWop9BKCQnx3pVgGTq', 1, 'Luis', 'luis@gmail.com');
 
 INSERT INTO role (name) VALUES('ROLE_USER');
 INSERT INTO role (name) VALUES('ROLE_ADMIN');
